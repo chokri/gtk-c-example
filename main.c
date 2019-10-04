63c797cfb254b3ff575ba2da17b0ba9309451b8f
GSantos23 <santosgerson64@gmail.com>
2019-10-04 09:18:42 -0600

    Rename window -> Hactoberfest 2019

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 static void activate (GtkApplication* app, gpointer user_data){
   GtkWidget *window;
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_title (GTK_WINDOW (window), "Hacktoberfest 2019");
   gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
   gtk_widget_show_all (window);
 }
